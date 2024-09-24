fcda2b8898ba3356bb779049ee4b94d4b5549f45
Ashwin B N <abharathan-nettoor@myseneca.ca>
2024-09-23 23:51:29 -0400

    updated

--- server.js
+++ server.js
@@ -37,11 +37,13 @@ const mongoose = require('mongoose')
 const app = express();
 
 // Serving static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
+
 
 // Setting the view engine to EJS
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
+
 
 // Setting the HTTP port
 const HTTP_PORT = process.env.PORT || 8080;
